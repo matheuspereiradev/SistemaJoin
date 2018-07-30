7e4c233f13f8c276b50284d830f0582f05bb4708
matheus20111997 <matheuslima20111997@gmail.com>
2018-07-30 17:51:03 -0300

    Atualizaçao commit

--- app/src/main/java/com/app/join/sistemajoin/ViewCadastrarEscolaEndereco.java
+++ app/src/main/java/com/app/join/sistemajoin/ViewCadastrarEscolaEndereco.java
@@ -2,12 +2,16 @@ package com.app.join.sistemajoin;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class ViewCadastrarEscolaEndereco extends AppCompatActivity {
 
+    EditText ctEscCEP;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_cadastrar_escola_endereco);
+
+
     }
 }

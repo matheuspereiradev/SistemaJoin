8cc3b596e7bc1a42a6ec43d0d875b90c0a2137e5
matheus20111997 <matheuslima20111997@gmail.com>
2018-07-30 22:53:12 -0300

    Commit com telas de listar e cadastrar escola prontas

--- app/src/main/java/com/app/join/sistemajoin/ViewListarEscolas.java
+++ app/src/main/java/com/app/join/sistemajoin/ViewListarEscolas.java
@@ -2,12 +2,21 @@ package com.app.join.sistemajoin;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class ViewListarEscolas extends AppCompatActivity {
+
+    ListView listaEscolas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_listar_escolas);
+
+        //=========inicio da conexão entre java e xml
+        listaEscolas = (ListView) findViewById(R.id.lwEscolasCadastradas);
+        //fim da conexão xml e java ==========
+
+
     }
 }

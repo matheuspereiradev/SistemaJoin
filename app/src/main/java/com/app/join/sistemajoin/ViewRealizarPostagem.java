9bed56fbeb66ae0d35e8c6866f9728c63c2d0d85
matheus20111997 <matheuslima20111997@gmail.com>
2018-08-03 18:14:26 -0300

    Con

--- app/src/main/java/com/app/join/sistemajoin/ViewRealizarPostagem.java
+++ app/src/main/java/com/app/join/sistemajoin/ViewRealizarPostagem.java
@@ -1,13 +1,35 @@
 package com.app.join.sistemajoin;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class ViewRealizarPostagem extends AppCompatActivity {
 
+    EditText ctTituloPost,ctMsgPost;
+    Button btSelecionarAlunos;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_realizar_postagem);
+
+        //=======conexão java com xml
+        ctTituloPost=(EditText)findViewById(R.id.ctTituloPost);
+        ctMsgPost=(EditText)findViewById(R.id.ctMsgPost);
+        btSelecionarAlunos=(Button)findViewById(R.id.btSelecionarAlunos);
+        //fim da conexão=========
+
+        btSelecionarAlunos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(getBaseContext(),ViewSelecionarAlunos.class);
+                startActivity(i);
+            }
+        });
+
+
     }
 }

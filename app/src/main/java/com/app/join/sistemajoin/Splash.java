4f2eb560be28e33b7412feb3fc32da3935d162e5
meninaqjeffedoido <jef9296@gmail.com>
2018-08-03 18:23:02 -0300

    Tela Splash Concluída

---
+++ app/src/main/java/com/app/join/sistemajoin/Splash.java
@@ -0,0 +1,25 @@
+package com.app.join.sistemajoin;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splash extends AppCompatActivity implements Runnable {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Handler handler = new Handler();
+        handler.postDelayed(this, 4000);
+
+    }
+    @Override
+    public void run(){
+        startActivity(new Intent(this,MainActivity.class));
+        finish();
+    }
+
+}

ec61461c77ecfdbd5d874c740a32f46022d8bfff
matheus20111997 <matheuslima20111997@gmail.com>
2018-08-18 19:39:44 -0300

    Design concluido e linkado com java

--- app/src/main/java/com/app/join/sistemajoin/Activitys/ViewExibirInformacoesProfessor.java
+++ app/src/main/java/com/app/join/sistemajoin/Activitys/ViewExibirInformacoesProfessor.java
@@ -2,14 +2,30 @@ package com.app.join.sistemajoin.Activitys;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.app.join.sistemajoin.R;
 
 public class ViewExibirInformacoesProfessor extends AppCompatActivity {
 
+    TextView cmpNomeProf,cmpTelProf,cmpEmailProf,cmpCPFProf,cmpDataNascProf,cmpRGProf,cmpFormacaoProf,cmpLoginProf,cmpSenhaProf;
+    ImageButton btEditProf,btConfigProf,btExcluirProf;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_exibir_informacoes_professor);
+        cmpNomeProf=(TextView)findViewById(R.id.cmpNomeProf);
+        cmpTelProf=(TextView)findViewById(R.id.cmpTelProf);
+        cmpEmailProf=(TextView)findViewById(R.id.cmpEmailEsc);
+        cmpCPFProf=(TextView)findViewById(R.id.cmpCNPJEsc);
+        cmpDataNascProf=(TextView)findViewById(R.id.cmpDataNascProf);
+        cmpRGProf=(TextView)findViewById(R.id.cmpRGProf);
+        cmpFormacaoProf=(TextView)findViewById(R.id.cmpFormacaoProf);
+        cmpLoginProf=(TextView)findViewById(R.id.cmpLoginProf);
+        cmpSenhaProf=(TextView)findViewById(R.id.cmpSenhaProf);
+        btEditProf=(ImageButton)findViewById(R.id.btEditProf);
+        btConfigProf=(ImageButton) findViewById(R.id.btConfigProf);
+        btExcluirProf=(ImageButton)findViewById(R.id.btExcluirProf);
     }
 }
--- app/src/main/java/com/app/join/sistemajoin/Activitys/ViewListaProfessores.java
+++ app/src/main/java/com/app/join/sistemajoin/Activitys/ViewListaProfessores.java
@@ -2,14 +2,18 @@ package com.app.join.sistemajoin.Activitys;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 import com.app.join.sistemajoin.R;
 
 public class ViewListaProfessores extends AppCompatActivity {
 
+    ListView lwProfessoresCadastrados;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_lista_professores);
+
+        lwProfessoresCadastrados=(ListView)findViewById(R.id.lwProfessoresCadastrados);
     }
 }

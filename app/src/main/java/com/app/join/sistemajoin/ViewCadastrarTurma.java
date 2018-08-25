e8ebb07d95294827e1d7ee88ec15e1c47c036640
matheus20111997 <matheuslima20111997@gmail.com>
2018-08-25 13:47:04 -0300

    Correção d bug na referencia do TextColor na view ExibirInfromaçõesAluno

---
+++ app/src/main/java/com/app/join/sistemajoin/ViewCadastrarTurma.java
@@ -0,0 +1,13 @@
+package com.app.join.sistemajoin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ViewCadastrarTurma extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_cadastrar_turma);
+    }
+}

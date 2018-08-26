287bbab48a59d29e681d64e4d39de8e7a4aa3990
matheus20111997 <matheuslima20111997@gmail.com>
2018-08-26 15:24:05 -0300

    Telas finalizadas do crud Turmas

--- app/src/main/java/com/app/join/sistemajoin/Activitys/ViewListarTurmas.java
+++ app/src/main/java/com/app/join/sistemajoin/Activitys/ViewListarTurmas.java
@@ -2,14 +2,17 @@ package com.app.join.sistemajoin.Activitys;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 import com.app.join.sistemajoin.R;
 
 public class ViewListarTurmas extends AppCompatActivity {
 
+    ListView lvTurmas;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_listar_turmas);
+        lvTurmas=(ListView)findViewById(R.id.lvTurmas);
     }
 }

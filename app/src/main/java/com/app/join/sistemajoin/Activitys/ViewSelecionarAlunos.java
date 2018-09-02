1899aa859add31685dbb2150f9bf16894ff6a49c
matheus20111997 <matheuslima20111997@gmail.com>
2018-09-02 15:12:01 -0300

    Tela de seleção de alunos

--- app/src/main/java/com/app/join/sistemajoin/Activitys/ViewSelecionarAlunos.java
+++ app/src/main/java/com/app/join/sistemajoin/Activitys/ViewSelecionarAlunos.java
@@ -2,14 +2,18 @@ package com.app.join.sistemajoin.Activitys;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 import com.app.join.sistemajoin.R;
 
 public class ViewSelecionarAlunos extends AppCompatActivity {
 
+    ListView listaSelecaoAlunos;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_selecionar_alunos);
+
+        listaSelecaoAlunos=(ListView) findViewById(R.id.listaSelecaoAlunos);
     }
 }

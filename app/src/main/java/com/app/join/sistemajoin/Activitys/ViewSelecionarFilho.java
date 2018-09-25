ef9f3d9decce524f0ea519c9bca11d0a7c511107
matheus20111997 <matheuslima20111997@gmail.com>
2018-09-25 19:02:01 -0300

    view selecionar filho

---
+++ app/src/main/java/com/app/join/sistemajoin/Activitys/ViewSelecionarFilho.java
@@ -0,0 +1,15 @@
+package com.app.join.sistemajoin.Activitys;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.app.join.sistemajoin.R;
+
+public class ViewSelecionarFilho extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_selecionar_filho);
+    }
+}

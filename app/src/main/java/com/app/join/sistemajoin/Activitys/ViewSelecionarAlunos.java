d57406e0e44b6e2967b5d5461f7b47a8bf86a7e2
matheus20111997 <matheuslima20111997@gmail.com>
2018-10-30 19:16:15 -0300

    Criação da turma

--- app/src/main/java/com/app/join/sistemajoin/Activitys/ViewSelecionarAlunos.java
+++ app/src/main/java/com/app/join/sistemajoin/Activitys/ViewSelecionarAlunos.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 import com.app.join.sistemajoin.Adapter.AlunoAdapter;
 import com.app.join.sistemajoin.Model.Aluno;
@@ -21,6 +22,7 @@ import java.util.ArrayList;
 public class ViewSelecionarAlunos extends AppCompatActivity {
 
     private ListView listview;
+    private TextView tvTurmaDosAlunos;
     private ArrayAdapter<Aluno> adapter;
     private ArrayList<Aluno> lista;
     private Aluno aluno, variavel;
@@ -31,6 +33,8 @@ public class ViewSelecionarAlunos extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_selecionar_alunos);
+
+        tvTurmaDosAlunos=(TextView)findViewById(R.id.tvTurmaDosAlunos);
 
         lista = new ArrayList();
         listview = findViewById(R.id.lwSelecionaAluno);
